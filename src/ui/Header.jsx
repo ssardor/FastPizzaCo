c0342a6ca2b7a4747ae9454c7818f71b151b7f3d
Islom Yuldashev <islomyuldashev816@gmail.com>
2023-11-27 16:39:01 +0500

    pizzaa

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,8 +4,8 @@ import "../index.css";
 
 const Header = () => {
   return (
-    <header className="display: flex gap-6 border-b-2 border-black bg-pizza p-5 font-Robo justify-between">
-      <div className=" display: flex  gap-20">
+    <header className="flex gap-6 border-b-2 border-black bg-pizza p-5 font-Robo justify-between">
+      <div className="flex justify-center gap-36">
         <Link
           to="/"
           className="text-yellow-500 bg-black p-2 rounded-full px-4 py-3"
@@ -17,6 +17,12 @@ const Header = () => {
           className="text-yellow-500 bg-black p-2 rounded-full px-5 py-3"
         >
           Menu
+        </Link>
+        <Link
+          to="/order/new"
+          className="text-yellow-500 bg-black p-2 rounded-full px-5 py-3"
+        >
+          Order
         </Link>
       </div>
       <h1 className="py-1">Username</h1>
